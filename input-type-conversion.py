f3f0570bb78e60dcf4ed1b589efbecabd8e14d44
Leela Prajapat <leelaprajapat.mba@gmail..com>
2026-01-25 00:37:01 +0530

    list_basics,tc

---
+++ input-type-conversion.py
@@ -0,0 +1,26 @@
+rollno = "what is your rollno?"
+print(rollno) 
+print("dtype =", type(rollno))
+print("addr of  string rollno =", id(rollno))
+
+# data overwriting
+rollno = 22
+print(rollno) 
+print("dtype =", type(rollno))
+print("addr of  int rollno =", id(rollno))
+
+
+
+rollno = True
+print(rollno) 
+print("dtype =", type(rollno))
+print("addr of  bool rollno =", id(rollno))
+
+
+roll_no = "WHAT IS YOUR ROLL NO?"
+
+roll = int(input((roll_no).lower()))
+print("dtype =", type(roll)) 
+
+print("Roll_no_inlower =", roll)
+    
---
+++ list_basic-1.py
@@ -0,0 +1,19 @@
+names=["Leela","Shubham","Manish","Ganesh"]
+
+print(names)
+print(type(names))
+ 
+ 
+elements=[1,34,56,87,False,True]
+
+print(elements[0])
+print(elements[1])
+
+print(elements[1:4])
+print(len(elements))             
+
+
+#list in python are mutable 
+elements[3]=99
+print(elements)
+
