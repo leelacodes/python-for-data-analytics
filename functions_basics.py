7110214b3f7881adc017d02e2180db6077432e46
Leela Prajapat <leelaprajapat.mba@gmail..com>
2026-02-03 01:06:10 +0530

    basic python

---
+++ functions_basics.py
@@ -0,0 +1,12 @@
+#print("good morning ")
+#print("how are you ")
+#print("thank you")
+
+#reusable code can be written in single blocks 
+
+def greet():
+    print("good morning")
+    print("how are you ")
+    print("thank you")
+
+greet()
---
+++ loops_basics.py
@@ -0,0 +1,39 @@
+#items=["apples",34,"banana","oranges"]
+
+#for item in items
+   # print(items)
+
+    #for loops 
+   # for i in range(0,10):
+       # print(i)
+
+#print i in range (0,10,2):
+
+
+
+#print i in range( 0,10,3):
+
+# for char in "LEELA":
+#     print(char)
+
+#     #while loop 
+#     count=1
+#     while(count<=5):
+#         print(count)
+#         count+=1
+#         print("END OF PROGRAME")
+
+
+#BREAK AND CONTINUE 
+# for i in range(0,10,2):
+#     if(i==5):
+#         break
+#     print(i)
+# print("end of loop!")
+
+for i in range(0,10,2):   #list = [0,1,2,3,4,5,6,7,8,9,10]
+    if (i==5):
+        continue
+
+    print(i)
+    print ("end of the loop")
